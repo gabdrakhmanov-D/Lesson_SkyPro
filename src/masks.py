4b3a992cdbb3991aef1f868075fd1cb0f9e5298b
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-08 20:50:04 +0500

    adding the masks module to the comit

---
+++ src/masks.py
@@ -0,0 +1,33 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Принимает на вход номер карты и возвращает ее маску."""
+
+    card_number_str = str(card_number)
+    hide_number_card = []
+    i = 0
+
+    for num in card_number_str:
+
+        if i <= 6:  # в этом условии в новый список добавляем первые 6 цифр карты
+            hide_number_card.append(num)
+            i += 1
+            if i % 4 == 0:
+                hide_number_card.append(" ")  # после 4 символов подряд вставляется пробел
+
+        elif i > 6 and ((len(card_number_str) - i) > 4):  # после шести цифр и до последних четырех вставляем звездочки
+            hide_number_card.append("*")
+            i += 1
+            if i % 4 == 0:
+                hide_number_card.append(" ")
+
+        else:  # в этом условии вставляем последние 4 цифры
+            hide_number_card.append(num)
+            i += 1
+            if i % 4 == 0:
+                hide_number_card.append(" ")
+
+    return "".join(hide_number_card)
+
+
+def get_mask_account(account_number: int) -> str:
+    """Принимает на вход номер счета и возвращает его маску."""
+    return f'**{str(account_number)[-4:]}'
