08f896582918ee46fb878d548c255db389545344
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-15 23:10:26 +0500

    Used Flake8, Black, Isort, mypy

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,29 @@
 from typing import Union
 
 
-def filter_by_state(list_dict: list[dict[str, Union[int, str]]], state='EXECUTED') -> list[dict[str, Union[int, str]]]:
-    """Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению"""
+def filter_by_state(
+    list_dict: list[dict[str, Union[int, str]]], state="EXECUTED"
+) -> list[dict[str, Union[int, str]]]:
+    """Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению"""
     new_list = []
     for i in list_dict:
-        if i['state'] == state:
+        if i["state"] == state:
             new_list.append(i)
     return new_list
 
 
-def sort_by_date(list_dict: list[dict[str, Union[int, str]]], sorting=True) -> list[dict[str, Union[int, str]]]:
+def sort_by_date(
+    list_dict: list[dict[str, Union[int, str]]], sorting=True
+) -> list[dict[str, Union[int, str]]]:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате."""
-    from widget import get_date
     from datetime import datetime
 
-    return sorted(list_dict, key=lambda date: datetime.strptime(get_date(date['date']), '%d.%m.%Y'), reverse=sorting)
+    from widget import get_date
+
+    return sorted(
+        list_dict,
+        key=lambda date: datetime.strptime(get_date(date["date"]), "%d.%m.%Y"),
+        reverse=sorting,
+    )
