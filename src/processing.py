e35deba0e5d1ff45e882bdf4f24279b1a0e36a8f
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-15 10:20:35 +0500

    The filter_by_state function is finished and tested

--- src/processing.py
+++ src/processing.py
@@ -4,7 +4,11 @@ from typing import Union
 def filter_by_state(list_dict:list[dict[str,Union[int,str]]], state= 'EXECUTED') -> list[dict[str,Union[int,str]]]:
     '''Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению'''
-    pass
+    new_list = []
+    for i in list_dict:
+        if i['state'] == state:
+            new_list.append(i)
+    return new_list
 
 
 def sort_by_date(list_dict:list[dict[str,Union[int,str]]], sorting = True) -> list[dict[str,Union[int,str]]]:
