0d68bd2304a68ebac2589b30d21f3b4520200c29
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-15 22:42:27 +0500

    Added an auxiliary function for sorting

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,9 @@
 from typing import Union
 
 
-def filter_by_state(list_dict:list[dict[str,Union[int,str]]], state= 'EXECUTED') -> list[dict[str,Union[int,str]]]:
-    '''Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению'''
+def filter_by_state(list_dict: list[dict[str, Union[int, str]]], state='EXECUTED') -> list[dict[str, Union[int, str]]]:
+    """Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей, содержащий только те словари, у которых ключ
+state соответствует указанному значению"""
     new_list = []
     for i in list_dict:
         if i['state'] == state:
@@ -11,8 +11,21 @@ state соответствует указанному значению'''
     return new_list
 
 
-def sort_by_date(list_dict:list[dict[str,Union[int,str]]], sorting = True) -> list[dict[str,Union[int,str]]]:
-    '''Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date).'''
-    return sorted(list_dict, key=lambda date: date['date'], reverse= sorting)
+def sort_by_date(list_dict: list[dict[str, Union[int, str]]], sorting=True) -> list[dict[str, Union[int, str]]]:
+    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список, отсортированный по дате."""
+    from widget import get_date
+    def key_sorting(date:str) -> tuple[int, int, int]:
+        """"""
+        tmp_list = get_date(date).split('.')
+        return int(tmp_list[2]), int(tmp_list[1]), int(tmp_list[0])
 
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    return sorted(list_dict, key=lambda date: key_sorting(date['date']), reverse=sorting)
+
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+# list_1 = [[11, 7, 2019], [12, 9, 2018], [10, 8, 2020], [13, 9, 2018]]
+# print(sorted(list_1, reverse=True))
