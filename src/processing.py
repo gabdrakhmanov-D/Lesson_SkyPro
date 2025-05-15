730b93528e8311ef1a1fdb0f281c41727c3980a5
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-15 23:24:38 +0500

    add list comprehension in func filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -2,19 +2,19 @@ from typing import Union
 
 
 def filter_by_state(
-    list_dict: list[dict[str, Union[int, str]]], state="EXECUTED"
+        list_dict: list[dict[str, Union[int, str]]], state="EXECUTED"
 ) -> list[dict[str, Union[int, str]]]:
     """Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению"""
-    new_list = []
-    for i in list_dict:
-        if i["state"] == state:
-            new_list.append(i)
+    new_list = [i for i in list_dict if i["state"] == state]
+    # for i in list_dict:
+    #     if i["state"] == state:
+    #         new_list.append(i)
     return new_list
 
 
 def sort_by_date(
-    list_dict: list[dict[str, Union[int, str]]], sorting=True
+        list_dict: list[dict[str, Union[int, str]]], sorting=True
 ) -> list[dict[str, Union[int, str]]]:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате."""
