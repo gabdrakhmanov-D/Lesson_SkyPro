3963930b2e14f3fa719fa8f8c6b0e493a11b96e9
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-15 10:30:26 +0500

    add sorted in sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -13,4 +13,6 @@ state соответствует указанному значению'''
 
 def sort_by_date(list_dict:list[dict[str,Union[int,str]]], sorting = True) -> list[dict[str,Union[int,str]]]:
     '''Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date).'''
-    pass
+    return sorted(list_dict, key=lambda date: date['date'], reverse= sorting)
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
