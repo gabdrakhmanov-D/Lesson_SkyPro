ed4925c0c9b0fa6924b65f19486d9d1c61d853d3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-23 22:19:31 +0500

    added fixes to feature get_mask_card_number

--- src/masks.py
+++ src/masks.py
@@ -1,33 +1,38 @@
 def get_mask_card_number(card_number: int) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
+    if card_number:
+        card_number_str = str(card_number)
+        hide_number_card = []
+        i = 0
 
-    card_number_str = str(card_number)
-    hide_number_card = []
-    i = 0
+        for num in card_number_str:
 
-    for num in card_number_str:
+            if i <= 5:  # в этом условии в новый список добавляем первые 6 цифр карты
+                hide_number_card.append(num)
+                i += 1
+                if i % 4 == 0:
+                    hide_number_card.append(" ")  # после 4 символов подряд вставляется пробел
 
-        if i <= 5:  # в этом условии в новый список добавляем первые 6 цифр карты
-            hide_number_card.append(num)
-            i += 1
-            if i % 4 == 0:
-                hide_number_card.append(" ")  # после 4 символов подряд вставляется пробел
+            elif i > 5 and ((len(card_number_str) - i) > 4):  # после шести цифр и до последних четырех вставляем звездочки
+                hide_number_card.append("*")
+                i += 1
+                if i % 4 == 0:
+                    hide_number_card.append(" ")
 
-        elif i > 5 and ((len(card_number_str) - i) > 4):  # после шести цифр и до последних четырех вставляем звездочки
-            hide_number_card.append("*")
-            i += 1
-            if i % 4 == 0:
-                hide_number_card.append(" ")
+            else:  # в этом условии вставляем последние 4 цифры
+                hide_number_card.append(num)
+                i += 1
+                if i % 4 == 0:
+                    hide_number_card.append(" ")
 
-        else:  # в этом условии вставляем последние 4 цифры
-            hide_number_card.append(num)
-            i += 1
-            if i % 4 == 0:
-                hide_number_card.append(" ")
+        if hide_number_card[-1] == ' ':
+            del hide_number_card[-1]
+        return "".join(hide_number_card)
 
-    return "".join(hide_number_card)
-
+    else:
+        return ""
 
 def get_mask_account(account_number: int) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
     return f'**{str(account_number)[-4:]}'
+
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,8 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-def test_mask_card():
-    pass
+import pytest
+
+@pytest.mark.parametrize('card_number, expected', [(1234567890123456, '1234 56** **** 3456'), (None, ''), (1234567890, '1234 5678 90'), (123456789012345678, '1234 56** **** **56 78'), (12345678901234567890, '1234 56** **** **** 7890')])
+
+def test_mask_card(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
