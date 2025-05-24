663daf00bb465953d3e63a2cf2bb5119eedff325
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-24 22:27:11 +0500

    Add raise TypeError, if empty incoming_data

--- src/widget.py
+++ src/widget.py
@@ -3,13 +3,15 @@
 def mask_account_card(incoming_data: str) -> str:
     """Функция, которая принимает номер счета или карты и возвращает строку с замаскированным номером."""
     from src import masks
-
-    tmp_list = incoming_data.split()
-    card_number = int(tmp_list[-1])
-    if "Счет" in tmp_list:
-        return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_account(card_number)}"
+    if incoming_data:
+        tmp_list = incoming_data.split()
+        card_number = int(tmp_list[-1])
+        if "Счет" in tmp_list:
+            return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_account(card_number)}"
+        else:
+            return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_card_number(card_number)}"
     else:
-        return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_card_number(card_number)}"
+        raise TypeError('Номер счета или карты не может быть пустым')
 
 
 def get_date(date: str) -> str:
