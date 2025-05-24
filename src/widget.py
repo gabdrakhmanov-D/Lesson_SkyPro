e747bd804f01c05455555b5d6a87b4e2232fc2a2
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-24 23:31:47 +0500

    added fixes to the function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@
 def mask_account_card(incoming_data: str) -> str:
     """Функция, которая принимает номер счета или карты и возвращает строку с замаскированным номером."""
     from src import masks
-    if incoming_data:
+    if incoming_data and isinstance(incoming_data, str):
         tmp_list = incoming_data.split()
         card_number = int(tmp_list[-1])
         if "Счет" in tmp_list:
@@ -11,7 +11,7 @@ def mask_account_card(incoming_data: str) -> str:
         else:
             return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_card_number(card_number)}"
     else:
-        raise TypeError('Номер счета или карты не может быть пустым')
+        raise TypeError('Номер счета или карты не может состоять из пустой строки!')
 
 
 def get_date(date: str) -> str:
