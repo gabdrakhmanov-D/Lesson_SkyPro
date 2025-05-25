1ff29981e978d351e2012b21c286f91f03e42eff
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-25 23:31:03 +0500

    added checking for numbers and symbols

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,21 @@
+from argparse import ArgumentError
 
 
-def mask_account_card(incoming_data: str) -> str:
+def mask_account_card(incoming_data: str = None) -> str:
     """Функция, которая принимает номер счета или карты и возвращает строку с замаскированным номером."""
     from src import masks
     if incoming_data and isinstance(incoming_data, str):
         tmp_list = incoming_data.split()
-        card_number = int(tmp_list[-1])
-        if "Счет" in tmp_list:
-            return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_account(card_number)}"
+        if any(symbols.isalpha() for symbols in tmp_list[:-1]) and any(symbols.isdigit() for symbols in tmp_list[-1]):# проверяет, состоит ли строка из букв и цифр
+            card_number = int(tmp_list[-1])
+            if "Счет" in tmp_list:
+                return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_account(card_number)}"
+            else:
+                return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_card_number(card_number)}"
         else:
-            return f"{' '.join(tmp_list[0:-1])} {masks.get_mask_card_number(card_number)}"
+            raise ValueError('Некорректный номер карты или счета')
     else:
-        raise TypeError('Номер счета или карты не может состоять из пустой строки!')
-
+        raise TypeError('Номер счета или карты может состоять только из строки!')
 
 def get_date(date: str) -> str:
     """Функция, которая изменяет формат даты"""
