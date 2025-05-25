ee0f8d318a6ce764e5b40d7ec2f88b00c8334f15
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-26 01:02:21 +0500

    add test wrong_format_date

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -15,7 +15,7 @@ def test_mask_account_card_empty():
     """Тест если не пришли никакие данные"""
     with pytest.raises(TypeError) as exc_info:
         mask_account_card()
-    assert str(exc_info.value) == "Номер счета или карты не может состоять из пустой строки!"
+    assert str(exc_info.value) == "Номер счета или карты может состоять только из строки!"
 
 @pytest.mark.parametrize('incoming_data', [12345657, ['Счет', '132456465454']])
 def test_mask_account_wrong_data(incoming_data):
@@ -30,13 +30,19 @@ def test_mask_account_card_missing_data(incoming_data):
     assert str(exc_info.value) == 'Некорректный номер карты или счета'
 
 #Тест функции get_date()
-@pytest.mark.parametrize('date, expected',[("2024-03-11T02:26:18.671407", "11.03.2024"),
-                                           ("2025-04-12T02:26:18.671407", "12.04.2025"),
-                                           ("2023-12-01T02:26:18.671407", "01.12.2023")])
+@pytest.mark.parametrize('date, expected',[('2024-03-11T02:26:18.671407', '11.03.2024'),
+                                           ('2025-04-12T02:26:18.671407', '12.04.2025'),
+                                           ('2023-12-01T02:26:18.671407', '01.12.2023')])
 def test_get_date(date,expected):
     assert get_date(date) == expected
 
 def test_get_date_empty_date():
     with pytest.raises(ValueError) as exc_info:
         get_date()
-    assert str(exc_info.value) == "Дата не может быть пустой"
+    assert str(exc_info.value) == 'Дата не может быть пустой'
+
+@pytest.mark.parametrize('date', ['2024.12.10', '2024-april-01', '01/01/2028','1 may 2025', 'просто строка без даты'])
+def test_get_date_wrong_format_date(date):
+    with pytest.raises(ValueError) as exc_info:
+        get_date(date)
+    assert str(exc_info.value) == 'Неверный формат даты'
