6d972289e88370fc6ef26a60f75a7a3f9406fffa
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-27 00:16:32 +0500

    add test sort_by_same_date

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,7 +5,8 @@ from src.processing import  filter_by_state, sort_by_date
 @pytest.mark.parametrize('state, expected', [('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
                                              ('CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])]
+                                                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+                                             ]
                         )
 def test_filter_by_state(dict_with_examples, state, expected):
     assert filter_by_state(dict_with_examples, state) == expected
@@ -24,6 +25,19 @@ def test_filter_by_state_no_state(dict_no_state_executed):
                                               (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                                                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                                                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                                       {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])])
+                                                       {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])
+                                              ]
+                         )
 def test_sort_by_date(dict_with_examples, sorting, expected):
     assert sort_by_date(dict_with_examples, sorting) == expected
+
+@pytest.mark.parametrize('sorting, expected', [(True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512363'},
+                                                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+                                                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425571'}]),
+                                               (False, [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425571'},
+                                                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+                                                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512363'},
+                                                        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])])
+def test_sort_by_same_date(dict_same_date,sorting,expected):
+    assert sort_by_date(dict_same_date, sorting) == expected
