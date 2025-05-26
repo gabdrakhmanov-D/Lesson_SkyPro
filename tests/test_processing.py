749d3942002b413b647a2f70980773fdd7062d6c
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-26 23:13:26 +0500

    added date matching check for the specified template

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import  filter_by_state
+from src.processing import  filter_by_state, sort_by_date
 
 @pytest.mark.parametrize('state, expected', [('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
@@ -13,3 +13,17 @@ def test_filter_by_state(dict_with_examples, state, expected):
 def test_filter_by_state_default(dict_with_examples):
     assert filter_by_state(dict_with_examples) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+def test_filter_by_state_no_state(dict_no_state_executed):
+    assert filter_by_state(dict_no_state_executed) == []
+
+@pytest.mark.parametrize('sorting, expected',[(True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                                              (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                                       {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])])
+def test_sort_by_date(dict_with_examples, sorting, expected):
+    assert sort_by_date(dict_with_examples, sorting) == expected
