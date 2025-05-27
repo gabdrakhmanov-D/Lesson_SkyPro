b97411f949de42b03c53d994cd2b0219d716a227
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-27 21:46:30 +0500

    add dict wrong_date_format

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,10 @@ def dict_same_date():
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512363'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425571'}]
+
+@pytest.fixture
+def dict_wrong_date_format():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '21 февраля 2025'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '12-12-2025'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '03.03.25'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': ''}]
