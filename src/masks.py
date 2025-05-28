abad7522c9c0b4792abe55cd9bf61687fe33d1e3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-28 23:52:04 +0500

    functions have been redesigned, account length checks of 16 and 20 have been added

--- src/masks.py
+++ src/masks.py
@@ -1,45 +1,13 @@
-def get_mask_card_number(card_number: int = None) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
-    if card_number:
-        card_number_str = str(card_number)
-        hide_number_card = []
-        i = 0
-
-        for num in card_number_str:
-
-            if i <= 5:  # в этом условии в новый список добавляем первые 6 цифр карты
-                hide_number_card.append(num)
-                i += 1
-                if i % 4 == 0:
-                    hide_number_card.append(
-                        " "
-                    )  # после 4 символов подряд вставляется пробел
-
-            elif i > 5 and (
-                (len(card_number_str) - i) > 4
-            ):  # после шести цифр и до последних четырех вставляем звездочки
-                hide_number_card.append("*")
-                i += 1
-                if i % 4 == 0:
-                    hide_number_card.append(" ")
-
-            else:  # в этом условии вставляем последние 4 цифры
-                hide_number_card.append(num)
-                i += 1
-                if i % 4 == 0:
-                    hide_number_card.append(" ")
-
-        if hide_number_card[-1] == " ":
-            del hide_number_card[-1]
-        return "".join(hide_number_card)
-
-    else:
-        raise ValueError("Номер карты не может быть пустым")
+    if not card_number.isdigit() or len(card_number) != 16:
+        raise ValueError('Номер карты должен состоять из 16 цифр!')
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
-def get_mask_account(account_number: int = None) -> str:
+def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
-    if account_number:
-        return f"**{str(account_number)[-4:]}"
-    else:
-        raise ValueError("Номер счета не может быть пустым")
+    if not account_number.isdigit() or len(account_number) != 20:
+        raise ValueError("Номер счета должен состоять из 20 цифр!")
+    return f"**{str(account_number)[-4:]}"
+
