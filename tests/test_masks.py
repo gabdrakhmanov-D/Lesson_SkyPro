2a4906b6a66fff8363e5d217cf924f8a523c46d3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-05-28 23:53:10 +0500

    removed tests with empty data and incorrect input data formats

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,29 +6,31 @@ from src.masks import get_mask_account, get_mask_card_number
 @pytest.mark.parametrize(
     "card_number, expected",
     [
-        (1234567890123456, "1234 56** **** 3456"),
-        (1234567890, "1234 5678 90"),
-        (123456789012345678, "1234 56** **** **56 78"),
-        (12345678901234567890, "1234 56** **** **** 7890"),
+        ("1234567890123456", "1234 56** **** 3456"),
+        ("7000792289606361", "7000 79** **** 6361"),
     ],
 )
 def test_mask_card(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
-
-def test_mask_card_empty_number():
+@pytest.mark.parametrize("card_number", ["123456789012345", "1245", "card number"])
+def test_mask_card_wrong_number(card_number):
     with pytest.raises(ValueError):
-        get_mask_card_number()
+        get_mask_card_number(card_number)
 
 
 @pytest.mark.parametrize(
     "account_number, expected",
-    [(73654108430135874305, "**4305"), (123456, "**3456"), (456, "**456")],
+    [
+        ("73654108430135874305", "**4305"),
+        ("73654108430135871111", "**1111"),
+        ("73654108430135807777", "**7777")],
 )
 def test_mask_account(account_number, expected):
     assert get_mask_account(account_number) == expected
 
 
-def test_mask_account_empty_number():
+@pytest.mark.parametrize("account_number", ["736541084301", "9876543210", "account_number"])
+def test_mask_account_empty_number(account_number):
     with pytest.raises(ValueError):
-        get_mask_account()
+        get_mask_account(account_number)
