dfe3efd7f2b88d2fe61b06952bdc600d9289a277
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-06 22:34:19 +0500

    filter_by_currency now outputs an iterator

--- src/generators.py
+++ src/generators.py
@@ -4,9 +4,4 @@ from typing import Union, Any, Generator
 def filter_by_currency(list_transact: list[dict[str, int | str]], currency) -> Generator[
     dict[str, int | str], Any, None]:
     """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
-    try:
-        for dict_in_list_transact in list_transact:
-            if dict_in_list_transact["operationAmount"]["currency"]["code"] == currency:
-                yield dict_in_list_transact
-    except StopIteration:
-        print('нет совпадений')
+    return (dict_in_list_transact for dict_in_list_transact in list_transact if dict_in_list_transact["operationAmount"]["currency"]["code"] == currency)
