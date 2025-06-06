269315c8cc20ef10127f63e1b6f5fa928aa1e9ef
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-06 20:56:19 +0500

    filter_by_currency is finished ver 1.0

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,12 @@
-from typing import Union
+from typing import Union, Any, Generator
 
 
-def filter_by_currency(list_transact: list[dict[str, Union[int, str]]]):
-    pass
+def filter_by_currency(list_transact: list[dict[str, int | str]], currency) -> Generator[
+    dict[str, int | str], Any, None]:
+    """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
+    try:
+        for dict_in_list_transact in list_transact:
+            if dict_in_list_transact["operationAmount"]["currency"]["code"] == currency:
+                yield dict_in_list_transact
+    except StopIteration:
+        print('нет совпадений')
