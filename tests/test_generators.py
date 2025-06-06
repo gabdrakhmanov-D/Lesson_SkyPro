940b30258d3fa41f5151c9a54518a5a62f83738e
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-06 21:08:51 +0500

    examples and tests have been created for the function filter_by_currency

---
+++ tests/test_generators.py
@@ -0,0 +1,53 @@
+from src.generators import filter_by_currency
+
+
+def test_generators(examples_for_generators):
+    generator_usd = filter_by_currency(examples_for_generators, "USD")
+    generator_rub =  filter_by_currency(examples_for_generators, "RUB")
+    generator_none = filter_by_currency(examples_for_generators, "EUR")
+    assert  next(generator_usd)== {
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      }
+    assert next(generator_usd) == {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       }
+    assert next(generator_rub) == {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }
+    assert next(generator_none) == 'нет такой валюты в списке'
