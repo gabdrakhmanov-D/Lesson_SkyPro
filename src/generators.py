4ddf043fe8773828bedb1464ef8f1ea71edaa1f9
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-07 23:02:36 +0500

    the code is formatted according to PEP-8

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,16 @@
-from typing import Union, Any, Generator
+from typing import Any, Generator
 
 
-def filter_by_currency(list_transact: list[dict[str, int | str]], currency) -> Generator[
-    dict[str, int | str], Any, None]:
-    """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
-    return (dict_in_list_transact for dict_in_list_transact in list_transact if dict_in_list_transact["operationAmount"]["currency"]["code"] == currency)
+def filter_by_currency(
+    list_transact: list[dict[str, int | str]], currency
+) -> Generator[dict[str, int | str], Any, None]:
+    """Функция возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD)."""
+    return (
+        dict_in_list_transact
+        for dict_in_list_transact in list_transact
+        if dict_in_list_transact["operationAmount"]["currency"]["code"] == currency
+    )
 
 
 def transaction_descriptions(list_transact: list[dict[str, int | str]]):
@@ -13,9 +19,11 @@ def transaction_descriptions(list_transact: list[dict[str, int | str]]):
         yield dict_in_list_transact["description"]
 
 
-def card_number_generator(start_number: int, end_number: int) -> Generator[str, Any, None]:
+def card_number_generator(
+    start_number: int, end_number: int
+) -> Generator[str, Any, None]:
     """Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты."""
-    bank_card_number = ['0'] * 16
+    bank_card_number = ["0"] * 16
 
     if end_number >= start_number:
         number_of_digits = end_number - start_number + 1
@@ -23,7 +31,7 @@ def card_number_generator(start_number: int, end_number: int) -> Generator[str, 
         number_of_digits = start_number - end_number + 1
 
     for number in range(number_of_digits):
-        for digit_index in range(-len(str(start_number)),0):
+        for digit_index in range(-len(str(start_number)), 0):
             bank_card_number[digit_index] = str(start_number)[digit_index]
-        yield f'{''.join(bank_card_number[0:4])} {''.join(bank_card_number[4:8])} {''.join(bank_card_number[8:12])} {''.join(bank_card_number[12:])}'
+        yield f"{''.join(bank_card_number[0:4])} {''.join(bank_card_number[4:8])} {''.join(bank_card_number[8:12])} {''.join(bank_card_number[12:])}"
         start_number += 1
