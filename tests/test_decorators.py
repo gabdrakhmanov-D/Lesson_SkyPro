fbdb28622398f7aa840e36e3e139cd72bb985543
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-10 13:35:54 +0500

    corrections have been made and the code has been adjusted to PEP8

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -19,17 +19,17 @@ def test_log_error(capsys):
         return a / b
     test_func(5, 0)
     captured = capsys.readouterr()
-    for word in ["В функции test_func произошла ошибка.\n", "Входные аргументы:(5, 0).\n",
-                 "Начало выполнения функции: ", "Время ошибки ", "Сообщение ошибки: division by zero"]:
-        assert word in captured.out
+    assert captured.out == (f'В функции test_func произошла ошибка.\n'
+                            f'Входные аргументы:(5, 0).\n'
+                            f'Сообщение ошибки: division by zero\n')
 
 
 def test_log_write_to_file(tmp_path):
     os.chdir(tmp_path)
-
     @log('test')
     def test_func(a, b):
         return a / b
+
 
     test_func(5, 5)
     file = open('test.txt', 'r', encoding='utf-8')
@@ -48,6 +48,6 @@ def test_log_write_to_file_error(tmp_path):
     with open('test.txt', 'r', encoding='utf-8') as file:
         result = file.read()
 
-    for word in ["В функции test_func произошла ошибка.\n", "Входные аргументы:(5, 0).\n",
-                 "Начало выполнения функции: ", "Время ошибки ", "Сообщение ошибки: division by zero"]:
-        assert word in result
+    assert result == (f'В функции test_func произошла ошибка.\n'
+                            f'Входные аргументы:(5, 0).\n'
+                            f'Сообщение ошибки: division by zero')
