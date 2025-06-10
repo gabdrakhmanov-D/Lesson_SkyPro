6f98fc49ea0a97994f980faf907d11c0199f4476
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-10 13:32:46 +0500

    corrections have been made in accordance with PEP8

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,12 @@
 import time
 from functools import wraps
+from typing import Any, Callable
 
 
-def log(filename = None):
+def log(filename: str = None) -> Callable[[Any], Callable[[tuple[Any, ...], dict[str, Any]], Any | None]]:
+    """Декоратор, который автоматически логирует начало и конец выполнения функции,
+       а также ее результаты или возникшие ошибки. Принимает необязательный аргумент filename, который определяет,
+       куда будут записываться логи (в файл или в консоль):"""
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -23,12 +27,20 @@ def log(filename = None):
                 end_time = time.ctime()
                 if filename:
                     with open(f'{filename}.txt', 'w', encoding='utf-8') as file:
-                        file.write(f'В функции {function_name} произошла ошибка.\nВходные аргументы:{argument_func if argument_func else ''}'
-                                   f'{f', {kw_argument_func}' if kw_argument_func else ''}.\nНачало выполнения функции: {start_time}.\nВремя ошибки {end_time}.\nСообщение ошибки: {e}')
+                        file.write(f'В функции {function_name} произошла ошибка.\n'
+                                   f'Входные аргументы:{argument_func if argument_func else ''}'
+                                   f'{f', {kw_argument_func}' if kw_argument_func else ''}.\n'
+                                   f'Начало выполнения функции: {start_time}.\n'
+                                   f'Время ошибки {end_time}.\n'
+                                   f'Сообщение ошибки: {e}')
                     return None
                 else:
-                    print(f'В функции {function_name} произошла ошибка.\nВходные аргументы:{argument_func if argument_func else ''}'
-                            f'{f', {kw_argument_func}' if kw_argument_func else ''}.\nНачало выполнения функции: {start_time}.\nВремя ошибки {end_time}.\nСообщение ошибки: {e}')
+                    print(f'В функции {function_name} произошла ошибка.\n'
+                          f'Входные аргументы:{argument_func if argument_func else ''}'
+                          f'{f', {kw_argument_func}' if kw_argument_func else ''}.\n'
+                          f'Начало выполнения функции: {start_time}.\n'
+                          f'Время ошибки {end_time}.\n'
+                          f'Сообщение ошибки: {e}')
                     return None
         return wrapper
     return decorator
