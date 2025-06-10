c4e85f83aa6a491343505bb68e570bcea7504798
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-10 13:35:54 +0500

    tests corrections have been made in accordance with PEP8

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,15 +1,12 @@
 import os
 
-import pytest
 from src.decorators import log
-import tempfile
 
 
 def test_log(capsys):
     @log()
     def test_func(a, b):
         return a / b
-
 
     test_func(5, 5)
     captured = capsys.readouterr()
@@ -29,10 +26,10 @@ def test_log_error(capsys):
 
 def test_log_write_to_file(tmp_path):
     os.chdir(tmp_path)
+
     @log('test')
     def test_func(a, b):
         return a / b
-
 
     test_func(5, 5)
     file = open('test.txt', 'r', encoding='utf-8')
@@ -41,16 +38,16 @@ def test_log_write_to_file(tmp_path):
 
 
 def test_log_write_to_file_error(tmp_path):
-        os.chdir(tmp_path)
-        @log('test')
-        def test_func(a, b):
-            return a / b
+    os.chdir(tmp_path)
 
+    @log('test')
+    def test_func(a, b):
+        return a / b
 
-        test_func(5, 0)
-        with open('test.txt', 'r', encoding='utf-8') as file:
-            result = file.read()
+    test_func(5, 0)
+    with open('test.txt', 'r', encoding='utf-8') as file:
+        result = file.read()
 
-        for word in ["В функции test_func произошла ошибка.\n", "Входные аргументы:(5, 0).\n",
-                     "Начало выполнения функции: ", "Время ошибки ", "Сообщение ошибки: division by zero"]:
-            assert word in result
+    for word in ["В функции test_func произошла ошибка.\n", "Входные аргументы:(5, 0).\n",
+                 "Начало выполнения функции: ", "Время ошибки ", "Сообщение ошибки: division by zero"]:
+        assert word in result
