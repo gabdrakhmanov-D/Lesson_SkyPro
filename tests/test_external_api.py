cc182e9da8b9cc45bfec4e079039feae6f4b1b7e
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-17 00:08:42 +0500

    implemented tests using MOCK for get_transaction_amount

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,8 +1,58 @@
+from unittest.mock import patch, Mock
+
+import pytest
+
 from src.external_api import get_transaction_amount
 
 
-def test_get_transaction_amount(examples_for_generators):
+def test_get_transaction_amount_rub(examples_for_generators):
+    """Тестирование транзакции в рублях"""
     assert get_transaction_amount(examples_for_generators[2]) == 43318.34
 
-def test_get_transaction_amount_usd(examples_for_generators):
+
+@patch('requests.get')
+def test_get_transaction_amount_usd(mock_get, examples_for_generators):
+    """Тестирование транзакции в долларах"""
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"status":"200","message":"rates","data":{"USDRUB":"64.1824"}}
+    mock_get.return_value = mock_response
+
     assert get_transaction_amount(examples_for_generators[1]) == 5077786.08
+
+
+@patch('requests.get')
+def test_get_transaction_amount_eur(mock_get, examples_for_generators):
+    """Тестирование транзакции в евро"""
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"status":"200","message":"rates","data":{"EURRUB":"100"}}
+    mock_get.return_value = mock_response
+
+    assert get_transaction_amount({
+                                    "id": 743628025,
+                                    "state": "EXECUTED",
+                                    "date": "2018-06-04T06:59:55.424356",
+                                    "operationAmount": {
+                                      "amount": "1",
+                                      "currency": {
+                                        "name": "EUR",
+                                        "code": "EUR"
+                                      }
+                                    },
+                                    "description": "Перевод организации",
+                                    "from": "Счет 54883981902864782073",
+                                    "to": "Счет 61834060137088759145"
+                                    }) == 100.0
+
+
+@patch('requests.get')
+def test_get_transaction_amount_error(mock_get, examples_for_generators):
+    """Тестирование когда нет доступа к сайту"""
+    mock_response = Mock()
+    mock_response.status_code = 404
+    mock_response.json.return_value = {"status": "404", "message": "rates", "data": {"USDRUB": "64.1824"}}
+    mock_get.return_value = mock_response
+
+    with pytest.raises(Exception):
+        get_transaction_amount(examples_for_generators[1])
