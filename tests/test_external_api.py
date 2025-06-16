be0f9814984e5c31324847d3ca22532cfeb23d80
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-16 22:57:38 +0500

    the get_transaction_amount function makes a request to the site and returns the amount

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -3,3 +3,6 @@ from src.external_api import get_transaction_amount
 
 def test_get_transaction_amount(examples_for_generators):
     assert get_transaction_amount(examples_for_generators[2]) == 43318.34
+
+def test_get_transaction_amount_usd(examples_for_generators):
+    assert get_transaction_amount(examples_for_generators[1]) == 5077786.08
