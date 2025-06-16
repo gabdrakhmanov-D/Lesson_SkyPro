63bcb0c6184df4994d8acbf896fe3ab37877b9e8
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-16 22:57:38 +0500

    the get_transaction_amount function makes a request to the site and returns the amount

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,18 @@
-def get_transaction_amount(transaction: dict):
+import requests
+
+API_KEY = 'a3472b28d030be6a04930b3d75d0cf34'
+def get_transaction_amount(transaction: dict) -> float:
+    """Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+       Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
+       и конвертации суммы операции в рубли."""
+
     transact = float(transaction["operationAmount"]["amount"])
+    currency = transaction["operationAmount"]["currency"]["code"]
+    if currency in ["USD","EUR"]:
+        response = requests.get(f'https://currate.ru/api/?get=rates&pairs={currency}RUB&key={API_KEY}')
+        if response.status_code != 200:
+            raise Exception(f"Курс для конвертации не получен. Возможная причина: {response.reason}")
+        exchange_rate = float(response.json()['data'][f'{currency}RUB'])
+        amount = round(transact * exchange_rate, 2)
+        return amount
     return transact
