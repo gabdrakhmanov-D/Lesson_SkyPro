d9520c1b7fea2de1da59bb0f968812ad77e3d822
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-16 21:45:29 +0500

    create tests for get_dict_transactions

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ def get_dict_transactions(path_to_file: str) -> list:
     try:
         with open(path_to_file, "r", encoding='utf-8') as file:
             data = json.load(file)
+            if type(data) != list:
+                return []
         return data
     except Exception:
         return []
