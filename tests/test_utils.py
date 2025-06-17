d6af7eb277022a1d92180d6634e74606d514dd9b
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-18 01:05:46 +0500

    the tests for the get_dict_transactions function have been redone using Mock

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,46 +1,64 @@
+import json
+from unittest import mock
+from unittest.mock import patch, Mock
+
+import pytest
+
 from src.utils import get_dict_transactions
 
 
-def test_get_dict_transactions_str(temp_file_str):
+def test_get_dict_transactions_str():
     """Тестирование случая когда в JSON файле строка"""
-    assert get_dict_transactions(temp_file_str) == []
+    with patch('builtins.open',new_callable=mock.mock_open, read_data='{"name": "Alice", "age": 25}') as mock_data:
+        assert get_dict_transactions('test') == []
+        mock_data.assert_called_once_with('test', 'r', encoding='utf-8') # Проверяем, что функция open была вызвана правильно
 
 
-def test_get_dict_transactions_empty_list(temp_file_empty_list):
+def test_get_dict_transactions_empty_list():
     """Тестирование случая когда JSON файл пустой"""
-    assert get_dict_transactions(temp_file_empty_list) == []
+    with patch('builtins.open',new_callable=mock.mock_open, read_data='') as mock_data:
+        assert get_dict_transactions('test') == []
+        mock_data.assert_called_once_with('test', 'r', encoding='utf-8') # Проверяем, что функция open была вызвана правильно
 
 
-def test_get_dict_transactions_no_file():
+def test_get_dict_transactions_not_found():
     """Тестирования случая когда файл не найден"""
-    assert get_dict_transactions("abc") == []
+    with patch('builtins.open', side_effect=FileNotFoundError) as mock_data:
+        assert get_dict_transactions('test') == []
+        mock_data.assert_called_once_with('test', 'r', encoding='utf-8') # Проверяем, что функция open была вызвана правильно
 
-
-def test_get_dict_transactions(temp_json_file):
+@pytest.mark.parametrize("expected, test_list", [([{
+                            "id": 441945886,
+                            "state": "EXECUTED",
+                            "date": "2019-08-26T10:50:58.294041",
+                            "operationAmount": {
+                              "amount": "31957.58",
+                              "currency": {
+                                "name": "руб.",
+                                "code": "RUB"
+                              }
+                            },
+                            "description": "Перевод организации",
+                            "from": "Maestro 1596837868705199",
+                            "to": "Счет 64686473678894779589"
+                          }], '''[
+                          {
+                            "id": 441945886,
+                            "state": "EXECUTED",
+                            "date": "2019-08-26T10:50:58.294041",
+                            "operationAmount": {
+                              "amount": "31957.58",
+                              "currency": {
+                                "name": "руб.",
+                                "code": "RUB"
+                              }
+                            },
+                            "description": "Перевод организации",
+                            "from": "Maestro 1596837868705199",
+                            "to": "Счет 64686473678894779589"
+                          }]''')])
+def test_get_read_success(test_list, expected):
     """Тестирование успешного чтения файла"""
-    assert get_dict_transactions(temp_json_file) == [
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {
-                "amount": "31957.58",
-                "currency": {"name": "руб.", "code": "RUB"},
-            },
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        },
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {
-                "amount": "8221.37",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560",
-        },
-    ]
+    with patch('builtins.open',new_callable=mock.mock_open, read_data=f"{test_list}") as mock_data:
+        assert get_dict_transactions('test') == expected
+        mock_data.assert_called_once_with('test', 'r', encoding='utf-8') # Проверяем, что функция open была вызвана правильно
