a6b1b6960a9557abed69b195ce61587f09d58e0f
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-23 21:42:50 +0500

    create logging points

--- src/masks.py
+++ src/masks.py
@@ -14,12 +14,15 @@ def get_mask_card_number(card_number: str) -> str:
     if not card_number.isdigit() or len(card_number) != 16:
         logging.error(f'Ошибка: неверный формат номера карты')
         raise ValueError('Номер карты должен состоять из 16 цифр!')
-    logging.info(f'Возврат замаскированного номера карты')
+    logging.info(f'Возврат маски номера карты')
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
+    logging.info(f'Старт работы функции get_mask_account')
     if not account_number.isdigit() or len(account_number) != 20:
+        logging.error(f'Ошибка: неверный формат номера счета')
         raise ValueError("Номер счета должен состоять из 20 цифр!")
+    logging.info(f'Возврат маски счета')
     return f"**{str(account_number)[-4:]}"
