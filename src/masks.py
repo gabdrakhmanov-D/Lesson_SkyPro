e0a0b8b662df0de16d64aa5cbc7bde6ca91a9884
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-23 21:40:52 +0500

    create logger

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,20 @@
+import logging
+
+logger = logging.getLogger('masks')
+file_handler = logging.FileHandler('logs\masks.log')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
+    logging.info(f'Старт работы функции get_mask_card_number')
     if not card_number.isdigit() or len(card_number) != 16:
+        logging.error(f'Ошибка: неверный формат номера карты')
         raise ValueError('Номер карты должен состоять из 16 цифр!')
+    logging.info(f'Возврат замаскированного номера карты')
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
