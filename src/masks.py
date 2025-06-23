c5f37ad177855caed6b090ba050a43935fc4242c
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-23 22:20:48 +0500

    configuring logging

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,9 @@
 import logging
 
-logger = logging.getLogger('masks')
-file_handler = logging.FileHandler('logs\masks.log')
-file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+
+logger = logging.getLogger('mask')
+file_handler = logging.FileHandler('../logs/masks.log', encoding= 'utf-8', mode= 'w')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(funcName)s %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
@@ -10,19 +11,19 @@ logger.setLevel(logging.DEBUG)
 
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
-    logging.info(f'Старт работы функции get_mask_card_number')
+    logger.info(f'Старт работы функции')
     if not card_number.isdigit() or len(card_number) != 16:
-        logging.error(f'Ошибка: неверный формат номера карты')
+        logger.error(f'Ошибка: неверный формат номера карты')
         raise ValueError('Номер карты должен состоять из 16 цифр!')
-    logging.info(f'Возврат маски номера карты')
+    logger.info(f'Возврат маски номера карты')
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
-    logging.info(f'Старт работы функции get_mask_account')
+    logger.info(f'Старт работы функции')
     if not account_number.isdigit() or len(account_number) != 20:
-        logging.error(f'Ошибка: неверный формат номера счета')
+        logger.error(f'Ошибка: неверный формат номера счета')
         raise ValueError("Номер счета должен состоять из 20 цифр!")
-    logging.info(f'Возврат маски счета')
+    logger.info(f'Возврат маски счета')
     return f"**{str(account_number)[-4:]}"
