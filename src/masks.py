51bc5761bd4c70b5624e096ce8a5f6303b4bb868
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-23 22:36:29 +0500

    The code has been edited in accordance with pep-8

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,7 @@
 import logging
 
-
 logger = logging.getLogger('mask')
-file_handler = logging.FileHandler('../logs/masks.log', encoding= 'utf-8', mode= 'w')
+file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8', mode='w')
 file_formatter = logging.Formatter('%(asctime)s %(name)s %(funcName)s %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -11,19 +10,19 @@ logger.setLevel(logging.DEBUG)
 
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
-    logger.info(f'Старт работы функции')
+    logger.info('Старт работы функции')
     if not card_number.isdigit() or len(card_number) != 16:
-        logger.error(f'Ошибка: неверный формат номера карты')
+        logger.error('Ошибка: неверный формат номера карты')
         raise ValueError('Номер карты должен состоять из 16 цифр!')
-    logger.info(f'Возврат маски номера карты')
+    logger.info('Возврат маски номера карты')
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
-    logger.info(f'Старт работы функции')
+    logger.info('Старт работы функции')
     if not account_number.isdigit() or len(account_number) != 20:
-        logger.error(f'Ошибка: неверный формат номера счета')
+        logger.error('Ошибка: неверный формат номера счета')
         raise ValueError("Номер счета должен состоять из 20 цифр!")
-    logger.info(f'Возврат маски счета')
+    logger.info('Возврат маски счета')
     return f"**{str(account_number)[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import json
 import logging
 
-
 logger = logging.getLogger('utils')
-file_handler = logging.FileHandler('../logs/utils.log', encoding= 'utf-8', mode= 'w')
+file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8', mode='w')
 file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -14,14 +13,14 @@ def get_dict_transactions(path_to_file: str) -> list:
     """Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
        Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
     try:
-        logger.info(f'Старт работы функции')
+        logger.info('Старт работы функции')
         with open(path_to_file, "r", encoding='utf-8') as file:
             data = json.load(file)
             logger.info(f'Успешное открытие файла {path_to_file}')
             if type(data) is not list:
                 logger.warning(f'Файл {path_to_file} имеет тип отличный от list, возврат пустого списка')
                 return []
-        logger.info(f'Успешная обработка файла и возврат содержимого')
+        logger.info('Успешная обработка файла и возврат содержимого')
         return data
     except Exception as ex:
         logger.error(f'Ошибка в работе функции: {ex}')
