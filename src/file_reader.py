636d00bdc85051725dce9f1edf8e92b0fd4a8361
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-27 16:48:46 +0500

    csv_file_reader outputs a list of transactions

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,8 +1,17 @@
 import pandas as pd
+import csv
 
-def csv_file_reader():
-    pass
-
+def csv_file_reader(path_to_file: str) -> list:
+    with open(path_to_file, encoding='utf-8') as file:
+        csv_data = csv.DictReader(file, delimiter=';')
+        list_transactions = []
+        for row in csv_data:
+            list_transactions.append(row)
+        return list_transactions
 
 def excel_file_reader():
     pass
+
+if __name__ == '__main__':
+    a=csv_file_reader('../data/transactions.csv')
+    print(a)
