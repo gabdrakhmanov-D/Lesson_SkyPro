b7a19a616a7a0a0fd4aa81930c596fbe958f5eb3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-28 22:06:46 +0500

    add test_category_counter_no_key

--- tests/test_find_transact.py
+++ tests/test_find_transact.py
@@ -76,3 +76,34 @@ def test_category_counter_wrong_category(examples_for_generators, category):
 def test_category_counter_wrong_dict():
     """Тестирование случая передачи пустого списка для поиска категорий"""
     assert category_counter([], ["Перевод организации", "Перевод со счета на счет"]) == {}
+
+
+@pytest.mark.parametrize("list_dict, expected", [(
+        [
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702",
+            },
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "operationAmount": {
+                    "amount": "9824.07",
+                    "currency": {"name": "USD", "code": "USD"},
+                },
+                "description": "Перевод организации",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702",
+            }
+        ],
+            {
+                "Перевод организации": 1
+            }
+        )])
+def test_category_counter_no_key(list_dict, expected):
+    """Тестирование случая, когда в каком то из словарей нет ключа 'description'"""
+    assert category_counter(list_dict, ["Перевод организации"]) == expected
