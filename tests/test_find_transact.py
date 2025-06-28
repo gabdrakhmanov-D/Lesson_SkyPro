c246373c37d0ebafcc733ac7976532256f986415
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-06-28 21:23:07 +0500

    add test_get_required_dictionary_wrong_pattern

--- tests/test_find_transact.py
+++ tests/test_find_transact.py
@@ -5,6 +5,13 @@ from src.find_transact import get_required_dictionary, category_counter
 @pytest.mark.parametrize("pattern", ["Перевод организации", "перевод организации"])
 def test_get_required_dictionary(pattern, examples_for_generators):
     """Тестирование успешного поиска транзакций"""
-    assert get_required_dictionary(examples_for_generators, pattern) == [examples_for_generators[0], examples_for_generators[-1]]
+    assert get_required_dictionary(examples_for_generators, pattern) == [examples_for_generators[0],
+                                                                         examples_for_generators[-1]]
+
+
+@pytest.mark.parametrize("pattern", [21, ["Перевод организации"]])
+def test_get_required_dictionary_wrong_pattern(examples_for_generators, pattern):
+    """Тестирование некорректного ввода значения поиска"""
+    assert get_required_dictionary(examples_for_generators, pattern) == []
 
 
