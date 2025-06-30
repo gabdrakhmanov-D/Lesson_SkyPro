c18830fb8cfdcea92dc8a9b02efe4a4eda988469
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-07-01 00:18:47 +0500

    add fixes in main and test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,7 +35,7 @@ def test_main_xlsx(monkeypatch, example_csv_and_xlsx, answer_xlsx_executed):
           patch('main.excel_file_reader') as mock_get):
 
         mock_get.return_value = example_csv_and_xlsx
-        responses = iter(['3', 'EXECUTED', 'да', 'убыванию', 'да', 'да', 'счет'])
+        responses = iter(['3', 'EXECUTED', 'да', 'возрастанию', 'да', 'да', 'счет'])
         monkeypatch.setattr('builtins.input', lambda _: next(responses))
         main()
         assert captured_output.getvalue() == answer_xlsx_executed
@@ -45,9 +45,34 @@ def test_main_no_match(monkeypatch, examples_for_generators, answer_no_match):
     """Тест случая отсутствия совпадений"""
     captured_output = io.StringIO()
     with (patch('sys.stdout', new=captured_output),
-        patch('main.get_dict_transactions') as mock_get):
+          patch('main.get_dict_transactions') as mock_get):
         mock_get.return_value = examples_for_generators
         responses = iter(['1', 'pending', 'да', 'убыванию', 'да', 'да', 'счет'])
         monkeypatch.setattr('builtins.input', lambda _: next(responses))
         main()
         assert captured_output.getvalue() == answer_no_match
+
+
+def test_main_json_wrong_input(monkeypatch, examples_for_generators, incorrect_input_words):
+    """Тест успешного выполнения программы с файлом json, но некорректными вводными параметрами"""
+    captured_output = io.StringIO()
+    with (patch('sys.stdout', new=captured_output),
+          patch('main.get_dict_transactions') as mock_get):
+        mock_get.return_value = examples_for_generators
+        responses = iter(['1', 'EXECUTED','д', 'да', 'у', 'убыванию', 'да', 'д', 'да', 'счет'])
+        monkeypatch.setattr('builtins.input', lambda _: next(responses))
+        main()
+        assert captured_output.getvalue() == incorrect_input_words
+
+
+def test_xlsx_wrong_input(monkeypatch, example_csv_and_xlsx, high_sort_xlsx):
+    """Тест успешного выполнения программы с файлом xlsx и некорректными вводными параметрами"""
+    captured_output = io.StringIO()
+    with (patch('sys.stdout', new=captured_output),
+          patch('main.excel_file_reader') as mock_get):
+
+        mock_get.return_value = example_csv_and_xlsx
+        responses = iter(['5', '3', 'XECU','EXECUTED','д', 'нет', 'фы', 'нет', 'нет'])
+        monkeypatch.setattr('builtins.input', lambda _: next(responses))
+        main()
+        assert captured_output.getvalue() == high_sort_xlsx
